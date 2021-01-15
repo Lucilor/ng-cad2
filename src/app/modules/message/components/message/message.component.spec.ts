07b5a5985b1c8927d61dd2863067a16f430bbad9
Lucilor <lucilor@me.com>
2021-01-15 11:23:39 +0800

    ✨feat(MessageModule): editor

--- src/app/modules/message/components/message/message.component.spec.ts
+++ src/app/modules/message/components/message/message.component.spec.ts
@@ -5,6 +5,7 @@ import {MatDialogRef, MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
+import {QuillModule} from "ngx-quill";
 
 import {MessageComponent} from "./message.component";
 
@@ -15,7 +16,7 @@ describe("MessageComponent", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [MessageComponent],
-            imports: [FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, PerfectScrollbarModule],
+            imports: [FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, PerfectScrollbarModule, QuillModule],
             providers: [
                 {provide: MatDialogRef, useValue: {}},
                 {provide: MAT_DIALOG_DATA, useValue: {}}
