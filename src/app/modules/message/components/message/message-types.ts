46a341823d548eeb91a86bb1f29d5fd4d1c532b6
Lucilor <lucilor@me.com>
2021-01-16 11:58:27 +0800

    🐞fix(message): bugs

--- src/app/modules/message/components/message/message-types.ts
+++ src/app/modules/message/components/message/message-types.ts
@@ -1,5 +1,5 @@
 import {ValidatorFn} from "@angular/forms";
-import {ObjectOf} from "@lucilor/utils";
+import {ObjectOf} from "@src/app/utils";
 
 export interface BaseMessageData {
     title?: string;
