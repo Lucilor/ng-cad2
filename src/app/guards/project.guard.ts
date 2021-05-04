5b1145d35e4277a2088a3140e5fe1ce4003ef52c
Lucilor <lucilor@me.com>
2021-05-04 08:57:58 +0800

    🔧build(deps): update deps

--- src/app/guards/project.guard.ts
+++ src/app/guards/project.guard.ts
@@ -1,15 +1,14 @@
 import {Injectable} from "@angular/core";
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from "@angular/router";
+import {CanActivate, ActivatedRouteSnapshot, Router} from "@angular/router";
 import {MessageService} from "../modules/message/services/message.service";
-import {AppStatusService} from "../services/app-status.service";
 
 @Injectable({
     providedIn: "root"
 })
 export class ProjectGuard implements CanActivate {
-    constructor(private router: Router, private message: MessageService, private status: AppStatusService) {}
+    constructor(private router: Router, private message: MessageService) {}
 
-    async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    async canActivate(route: ActivatedRouteSnapshot) {
         const project = route.queryParams.project;
         if (!project) {
             const url = route.children[0]?.url.toString() || "/";
