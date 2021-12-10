8a9076993763ba3570d2228362dd851b3982c2b5
Lucilor <lucilor@me.com>
2021-12-10 11:43:04 +0800

    🔧 build(deps): update deps

--- src/app/views/index/index.component.spec.ts
+++ src/app/views/index/index.component.spec.ts
@@ -4,6 +4,7 @@ import {FormsModule} from "@angular/forms";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatIconModule} from "@angular/material/icon";
@@ -59,6 +60,7 @@ describe("IndexComponent", () => {
                 MatAutocompleteModule,
                 MatButtonModule,
                 MatCardModule,
+                MatCheckboxModule,
                 MatDividerModule,
                 MatExpansionModule,
                 MatIconModule,
