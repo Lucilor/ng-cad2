22c9f044aa790129049180f99fa2be604b06fe0e
Lucilor <lucilor@me.com>
2022-05-26 15:18:29 +0800

    🐞 fix(cad.utils): bug

--- src/app/cad.utils.ts
+++ src/app/cad.utils.ts
@@ -274,10 +274,10 @@ interface GetWrapedTextOptions {
 }
 const getWrapedTextOptions = (source: string, maxLength: number) => {
     const options: GetWrapedTextOptions = {maxLength};
-    if (source.match(/\d+(\.\d+)?[x×]?\d+(\.\d+)?/)) {
+    if (source.match(/\d+(\.\d+)?[x×]\d+(\.\d+)?/)) {
         options.minLength = 1;
         options.indent = 4;
-        options.separator = /[,.，。:；]/;
+        options.separator = /[,，。:；]/;
     }
     return options;
 };
