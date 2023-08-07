eff5e4067a49fb36a447c9cdcbc3f9c337f7f83c
Lucilor <lucilor@me.com>
2023-08-07 10:47:44 +0800

    🐞 fix(print-cad): 指定分类

--- src/app/views/print/print-cad.component.ts
+++ src/app/views/print/print-cad.component.ts
@@ -178,7 +178,8 @@ export class PrintCadComponent implements AfterViewInit, OnDestroy {
         }
         this.downloadUrl = responseData.url || null;
         this.printParams = {...this.printParams, ...responseData};
-        this.printParams.info.title = "算料单 " + this.printParams.codes.join("、");
+        const 指定分类 = queryParams.指定分类 || "算料单";
+        this.printParams.info.title = `${指定分类} ${this.printParams.codes.join("、")}`;
         document.title = this.printParams.info.title;
         const {codes, type} = this.printParams;
         if (codes.length === 1) {
